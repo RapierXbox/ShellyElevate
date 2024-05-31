d67b78913ae6a337ecfbb24dfde83f678747c9e9
RapierXbox <65401386+RapierXbox@users.noreply.github.com>
2024-05-31 14:55:12 +0200

    move chrome starting and add logging

--- app/src/main/java/me/rapierxbox/ShellyElevate/BootReceiver.java
+++ app/src/main/java/me/rapierxbox/ShellyElevate/BootReceiver.java
@@ -2,26 +2,20 @@ package me.rapierxbox.ShellyElevate;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.util.Log;
 
 import androidx.legacy.content.WakefulBroadcastReceiver;
-
 
 public class BootReceiver extends WakefulBroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Log.i("autostart", "Starting HTTP Server");
             Intent serviceIntent = new Intent(context, HttpServer.class);
             startWakefulService(context, serviceIntent);
 
-            String url = "http://homeassistant.local:8123";
-            Uri uri = Uri.parse(url);
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, uri);
-            browserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            browserIntent.setPackage("com.android.chrome");
-            browserIntent.putExtra("android.support.customtabs.extra.TITLE_VISIBILITY", 0);
-            browserIntent.putExtra("android.support.customtabs.extra.TINT_ACTION_BUTTON", false);
-            context.startActivity(browserIntent);
+            Log.i("autostart", "Starting Chrome");
+            new ChromeHelper(context).execute();
         }
     }
 }
