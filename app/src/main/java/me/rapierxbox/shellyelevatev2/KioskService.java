9d40fea6a08234c9b842246e6e887e5fa3f14449
RapierXbox <65401386+RapierXbox@users.noreply.github.com>
2026-04-21 20:05:47 +0200

    replace deprecated getRunningTasks with getAppTasks

--- app/src/main/java/me/rapierxbox/shellyelevatev2/KioskService.java
+++ app/src/main/java/me/rapierxbox/shellyelevatev2/KioskService.java
@@ -5,6 +5,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -97,14 +98,15 @@ public class KioskService extends Service {
 
 	private boolean isActivityAtTop(Class<?> activityClass) {
 		ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-		if (am != null) {
-			List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(Integer.MAX_VALUE);
-			for (ActivityManager.RunningTaskInfo task : tasks) {
-				if (task.topActivity != null &&
-						task.topActivity.getClassName().equals(activityClass.getName())) {
+		if (am == null) return false;
+		List<ActivityManager.AppTask> tasks = am.getAppTasks();
+		for (ActivityManager.AppTask task : tasks) {
+			try {
+				ComponentName top = task.getTaskInfo().topActivity;
+				if (top != null && top.getClassName().equals(activityClass.getName())) {
 					return true;
 				}
-			}
+			} catch (Exception ignored) {}
 		}
 		return false;
 	}
